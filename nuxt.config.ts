7bf9c2bb915fd6f8dbb360b60164a07f22fc3681
Muhammad Reza <mdotreza@gmail.com>
2025-07-13 15:02:08 +0700

    add work section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: true,
+  target: 'static', // needed for full static generation
+  content: {
+    documentDriven: true,
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: [
