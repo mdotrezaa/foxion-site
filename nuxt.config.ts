fa07c492f95bc04986dccce3c6ab65e9343be449
Muhammad Reza <mdotreza@gmail.com>
2025-07-15 20:16:33 +0700

    delete outline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  target: 'static',
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: [
